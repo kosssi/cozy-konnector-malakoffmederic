19e803d3867407083298b76f206587abae7124d5
doubleface <christophe@thiriot.xyz>
2017-10-13 12:08:10 +0200

    publish: More logs for LOGIN_FAILED error
    generated from commit 7103acca0f2a0fd51d7f4c4f7894ff1c2809c34a

--- index.js
+++ index.js
@@ -54368,6 +54368,7 @@ module.exports = new BaseKonnector(function fetch (fields) {
   })
   .then(body => {
     if (body.indexOf('LOGON_KO') > -1) {
+      console.log(body, 'LOGIN_FAILED error details')
       throw new Error('LOGIN_FAILED')
     }
     log('info', 'LOGGED_IN')
